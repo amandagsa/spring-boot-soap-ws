5f4bd8d27214923ea4390c2826328e60c0ba2748
Amanda Alves <amandagsal@gmail.com>
2023-04-25 14:49:40 -0300

    Atualiza bind classes

--- src/main/java/com/javatechie/spring/soap/api/loaneligibility/ObjectFactory.java
+++ src/main/java/com/javatechie/spring/soap/api/loaneligibility/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2023.04.25 às 11:14:52 AM BRT 
+// Gerado em: 2023.04.25 às 02:31:15 PM BRT 
 //
 
 
